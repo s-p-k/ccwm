36856b4585e3c7e02511c145a095195d7208649f
Ari Malinen <ari.malinen@gmail.com>
2013-08-03 22:23:58 +0300

    First functional release

--- README.md
+++ README.md
@@ -3,6 +3,31 @@ ccwm
 
 C CLI Wlan Manager
 
-Work in progress. Not usable yet!
-The goal is to create consistent cli wlan manager
-to handle connecting to all networks without any configuration.
+ccwm allows you to easily connect to any wlan access point.
+There is no need for configuration.
+This is work in progress but its already functional.
+
+usage
+====
+
+Usage: ccwm [options] -o/-a/-w/-W
+
+-h              Show help
+
+-i [interface]  Set interface (default: wlan0)
+-e [ssid]       Set SSID
+-f [freq]       Set frequency
+-k [key]        Set key/passphrase
+
+-s              Scan access points
+-S              Scan access points verbosely
+-I              Show interface info
+
+-o              Connect to ESS access point
+-a              Connect to IBSS access point
+-w              Connect to WEP encrypted access point
+-W              Connect to WPA encrypted access point
+
+changelog
+====
+03.08.2013 - First functional release.
--- ccwm.c
+++ ccwm.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <string.h>
 
 char **global_argv;
 
 void usage() {
-	printf("Usage: %s [options]\n\n", global_argv[0]);
+	printf("Usage: %s [options] -o/-a/-w/-W\n\n", global_argv[0]);
 
 	puts("-h              Show help\n");
 
 	puts("-i [interface]  Set interface (default: wlan0)");
-	puts("-e [essid]      Set essid");
+	puts("-e [ssid]       Set SSID");
 	puts("-f [freq]       Set frequency");
 	puts("-k [key]        Set key/passphrase\n");
 
@@ -19,8 +20,8 @@ void usage() {
 	puts("-S              Scan access points verbosely");
 	puts("-I              Show interface info\n");
 
-	puts("-o              Connect to ESS (open) access point");
-	puts("-a              Connect to IBSS (ad-hoc) access point");
+	puts("-o              Connect to ESS access point");
+	puts("-a              Connect to IBSS access point");
 	puts("-w              Connect to WEP encrypted access point");
 	puts("-W              Connect to WPA encrypted access point");
 }
@@ -30,7 +31,7 @@ void ifup(char *ifname) {
 	cmdline = malloc(36);
 	size_t s1 = strlen(ifname);
 	snprintf(cmdline, s1+16, "ip link set %s up", ifname);
-	printf("debug: %s\n", cmdline);
+	printf("cmd: %s\n", cmdline);
 	system(cmdline);
 	free(cmdline);
 }
@@ -42,12 +43,12 @@ void scan(char *ifname, int type) {
 
 	if (type == 0) {
 		snprintf(cmdline, s1+31, "iw dev %s scan | awk -f scan.awk", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 	else {
 		snprintf(cmdline, s1+13, "iw dev %s scan", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 
@@ -59,66 +60,68 @@ void info(char *ifname) {
 	cmdline = malloc(43);
 	size_t s1 = strlen(ifname);
 	snprintf(cmdline, s1+13, "iw dev %s info", ifname);
-	printf("debug: %s\n", cmdline);
+	printf("cmd: %s\n", cmdline);
 	system(cmdline);
 	snprintf(cmdline, s1+13, "iw dev %s link", ifname);
-	printf("debug: %s\n", cmdline);
+	printf("cmd: %s\n", cmdline);
 	system(cmdline);
 	snprintf(cmdline, s1+23, "iw dev %s get power_save", ifname);
-	printf("debug: %s\n", cmdline);
+	printf("cmd: %s\n", cmdline);
 	system(cmdline);
 	free(cmdline);
 }
 
-void connect(char *ifname, char *essid, int type) {
+void connect(char *ifname, char *essid, char *freq, char *key, int type) {
 	system("killall dhcpcd 2> /dev/null");
 	char *cmdline = "";
-	cmdline = malloc(93);
+	cmdline = malloc(100);
 	size_t s1 = strlen(ifname);
 	size_t s2 = strlen(essid);
+	size_t s3 = strlen(freq);
+	size_t s4 = strlen(key);
 
 	if (type < 3) {
 		snprintf(cmdline, s1+25, "iw dev %s set type managed", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 	else {
 		snprintf(cmdline, s1+22, "iw dev %s set type ibss", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 
 	if (type == 0) {
 		snprintf(cmdline, s1+s2+20, "iw dev %s connect -w %s", ifname, essid);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 		snprintf(cmdline, s1+8, "dhcpcd %s", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 	else if (type == 1) {
-		snprintf(cmdline, s1+s2+31, "iw dev %s connect -w %s keys 12345", ifname, essid);
-		printf("debug: %s\n", cmdline);
+		snprintf(cmdline, s1+s2+s4+26, "iw dev %s connect -w %s keys %s", ifname, essid, key);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 		snprintf(cmdline, s1+8, "dhcpcd %s", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 	else if (type == 2) {
-		snprintf(cmdline, s1+s2+53, "wpa_supplicant -B -i %s -c <(wpa_passphrase %s 12345678)",
-									ifname, essid);
-		printf("debug: %s\n", cmdline);
+		snprintf(cmdline, s1+s2+s4+45, "wpa_supplicant -B -i %s -c <(wpa_passphrase %s %s)",
+									ifname, essid, key);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 		snprintf(cmdline, s1+8, "dhcpcd %s", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 	else {
-		snprintf(cmdline, s1+s2+24, "iw dev %s ibss join %s 2457", ifname, essid);
-		printf("debug: %s\n", cmdline);
+		snprintf(cmdline, s1+s2+s3+20, "iw dev %s ibss join %s %s", ifname, essid, freq);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 		snprintf(cmdline, s1+8, "dhcpcd %s", ifname);
-		printf("debug: %s\n", cmdline);
+		printf("cmd: %s\n", cmdline);
 		system(cmdline);
 	}
 
@@ -130,14 +133,20 @@ int main(int argc, char *argv[]) {
 	int opt;
 	char *ifname = "wlan0";
 	char *essid = "";
-	char *key = "";
 	char *freq = "";
+	char *key = "";
+
+	uid_t uid=getuid();
+	if (uid != 0) {
+		puts("\nThis program needs to be run with root privileges!\n");
+		/* return 1; */
+	}
 
 	if (argc <= 1) {
 		usage();
 	}
 
-	while ((opt = getopt(argc, argv, "IsSowWahi:e:k:f:")) != -1) {
+	while ((opt = getopt(argc, argv, "IsSowWahi:e:f:k:")) != -1) {
 		switch (opt) {
 			case 'i':
 				ifname = optarg;
@@ -145,11 +154,11 @@ int main(int argc, char *argv[]) {
 			case 'e':
 				essid = optarg;
 				break;
-			case 'k':
-				key = optarg;
-				break;
 			case 'f':
 				freq = optarg;
+				break;
+			case 'k':
+				key = optarg;
 				break;
 			case 'I':
 				ifup(ifname);
@@ -164,20 +173,36 @@ int main(int argc, char *argv[]) {
 				scan(ifname, 1);
 				break;
 			case 'o':
+				if (essid[0] == '\0') {
+					printf("You must specify ssid!\n");
+					break;
+				}
 				ifup(ifname);
-				connect(ifname, essid, 0);
+				connect(ifname, essid, freq, key, 0);
 				break;
 			case 'w':
+				if (essid[0] == '\0' || key[0] == '\0') {
+					printf("You must specify ssid and key!\n");
+					break;
+				}
 				ifup(ifname);
-				connect(ifname, essid, 1);
+				connect(ifname, essid, freq, key, 1);
 				break;
 			case 'W':
+				if (essid[0] == '\0' || key[0] == '\0') {
+					printf("You must specify ssid and passphrase!\n");
+					break;
+				}
 				ifup(ifname);
-				connect(ifname, essid, 2);
+				connect(ifname, essid, freq, key, 2);
 				break;
 			case 'a':
+				if (essid[0] == '\0' || freq[0] == '\0') {
+					printf("You must specify ssid and freq!\n");
+					break;
+				}
 				ifup(ifname);
-				connect(ifname, essid, 3);
+				connect(ifname, essid, freq, key, 3);
 				break;
 			case 'h':
 			default:
